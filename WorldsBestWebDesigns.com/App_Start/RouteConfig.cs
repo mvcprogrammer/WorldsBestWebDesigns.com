4186bdc949619a1111661f51bf183dff957c43e4
sysop <sysop@WorldsBest>
2018-11-06 13:36:29 -0500

    Add .gitignore and .gitattributes.

---
+++ WorldsBestWebDesigns.com/App_Start/RouteConfig.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WorldsBestWebDesigns.com
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "SiteTechnology",
+                url: "site-technology",
+                defaults: new { controller = "SiteTechnology", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "ReachMe",
+                url: "contact-me",
+                defaults: new { controller = "ContactUs", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "ReachUs",
+                url: "contact-us",
+                defaults: new { controller = "ContactUs", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "AboutMe",
+                url: "about-me",
+                defaults: new { controller = "AboutUs", action = "Index" }
+            );
+
+
+            routes.MapRoute(
+                name: "AboutUs",
+                url: "about-us",
+                defaults: new { controller = "AboutUs", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Branding",
+                url: "branding",
+                defaults: new { controller = "Branding", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "SEO",
+                url: "search-engine-optimization",
+                defaults: new { controller = "SEO", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "MobileApps",
+                url: "mobile-apps",
+                defaults: new { controller = "MobileApps", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "WebApps",
+                url: "web-apps",
+                defaults: new { controller = "WebApps", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "SoftwareDesign",
+                url: "software-design",
+                defaults: new { controller = "SoftwareDesign", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "WebDesign",
+                url: "web-design",
+                defaults: new { controller = "WebDesign", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WorldsBestWebDesigns.com/Controllers/WorldsBestControllers.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorldsBestWebDesigns.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult flexiSlider()
+        {
+            return PartialView();
+        }
+
+        public ActionResult tagLine()
+        {
+            return PartialView();
+        }
+
+        public ActionResult intro()
+        {
+            return PartialView();
+        }
+
+        public ActionResult sliderAd()
+        {
+            return PartialView();
+        }
+
+        public ActionResult recentProjects()
+        {
+            return PartialView();
+        }
+
+        public ActionResult principalServices()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ourClients()
+        {
+            return PartialView();
+        }
+    }
+
+    public class CommonController : Controller
+    {
+        public ActionResult footer() => PartialView();
+        public ActionResult navbar() => PartialView();
+        public ActionResult scripts() => PartialView();
+    }
+
+    public class WebDesignController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult responsiveWebDesigns() => PartialView();
+    }
+
+    public class SoftwareDesignController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult softwareDesign() => PartialView();
+    }
+
+    public class WebAppsController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult webApps() => PartialView();
+    }
+
+    public class MobileAppsController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult mobileApps() => PartialView();
+    }
+
+    public class SEOController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult SEO() => PartialView();
+    }
+
+    public class BrandingController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult branding() => PartialView();
+    }
+
+    public class PortfolioController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult portfolio() => PartialView();
+        public ActionResult callToAction() => PartialView();
+    }
+
+    public class AboutUsController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult aboutUs() => PartialView();
+        public ActionResult moreAboutUs() => PartialView();
+    }
+
+    public class ContactUsController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult contactUs() => PartialView();
+    }
+
+    public class SiteTechnologyController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult breadcrumb() => PartialView();
+        public ActionResult aboutThis() => PartialView();
+    }
+}
